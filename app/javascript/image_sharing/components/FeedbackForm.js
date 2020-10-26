ff7fff6c2234328c5b245b54621f7effe2912aca
Noam Swisa <noam.swisa@appfolio.com>
2020-10-26 14:32:09 -0700

    ns - wip

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import post from '../utils/helper.js';
 
-const FeedbackForm = () => (
-  <Form style={{ width: '70%' }}>
-    <FormGroup>
-      <Label>Your Name:</Label>
-      <Input type='text' />
-    </FormGroup>
-    <FormGroup>
-      <Label>Comments:</Label>
-      <Input type='textarea' />
-    </FormGroup>
-    <Button color='primary'>Submit</Button>
-  </Form>
-);
+class FeedbackForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      comments: ''
+    };
+  }
+
+  onNameChange(name) {
+    this.setState({ name });
+  }
+
+  onCommentChange(comment) {
+    this.setState({ comments: comment });
+  }
+
+  submitFeedbackToBackend() {
+    const path = '/api/feedbacks';
+    post(path, this.state);
+  }
+
+  render() {
+    return (
+      <Form style={{ width: '70%' }}>
+        <FormGroup>
+          <Label>Your Name:</Label>
+          <Input onChange={event => this.onNameChange(`${event.target.value}`)} type='text' />
+        </FormGroup>
+        <FormGroup>
+          <Label>Comments:</Label>
+          <Input onChange={event => this.onCommentChange(`${event.target.value}`)} type='textarea' />
+        </FormGroup>
+        <Button color='primary' onClick={this.submitFeedbackToBackend()}>Submit</Button>
+      </Form>
+    );
+  }
+}
 
 export default FeedbackForm;
