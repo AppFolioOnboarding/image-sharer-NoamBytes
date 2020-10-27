fd1fb078e5c023464b5b3635d763907c6d1c6b5c
Noam Swisa <noam.swisa@appfolio.com>
2020-10-27 10:45:25 -0700

    ns - added controller for feedbacks

---
+++ test/controllers/feedbacks_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class FeedbacksControllerTest < ActionDispatch::IntegrationTest
+  def test_create_valid
+    assert_difference('Feedback.count', 1) do
+      post api_feedbacks_url, params: { feedback: { name: 'SomeName', comments: 'blah blah' } }
+    end
+    assert_response :success
+    assert_equal JSON.parse(response.body), { 'message' => 'Feedback successfully saved!' }
+  end
+
+  def test_create_invalid
+    assert_no_difference('Feedback.count') do
+      post api_feedbacks_url, params: { feedback: { name: '', comments: '' } }
+    end
+    assert_response :unprocessable_entity
+    assert_equal JSON.parse(response.body), { 'message' => 'Feedback was unable to save' }
+  end
+end
