a4fd4bcd41e8fa37b83855527cc5beae33f2b8f9
Noam Swisa <noam.swisa@appfolio.com>
2020-10-27 10:46:19 -0700

    ns - connected feedback form to backend

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import post from '../utils/helper.js';
+import { Alert, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { post } from '../utils/helper.js';
 
 class FeedbackForm extends React.Component {
   constructor() {
     super();
     this.state = {
       name: '',
-      comments: ''
+      comments: '',
+      alertStatus: undefined,
+      alertMessage: ''
     };
   }
 
@@ -21,22 +23,44 @@ class FeedbackForm extends React.Component {
 
   submitFeedbackToBackend() {
     const path = '/api/feedbacks';
-    post(path, this.state);
+    return post(path, { name: this.state.name, comments: this.state.comments }).then((response) => {
+      this.setState({ alertStatus: 'success', alertMessage: response.message });
+    }).catch((response) => {
+      this.setState({ alertStatus: 'danger', alertMessage: response.data.message });
+    }).finally(() => {
+      this.setState({
+        name: '',
+        comments: ''
+      });
+    });
   }
 
   render() {
     return (
-      <Form style={{ width: '70%' }}>
-        <FormGroup>
-          <Label>Your Name:</Label>
-          <Input onChange={event => this.onNameChange(`${event.target.value}`)} type='text' />
-        </FormGroup>
-        <FormGroup>
-          <Label>Comments:</Label>
-          <Input onChange={event => this.onCommentChange(`${event.target.value}`)} type='textarea' />
-        </FormGroup>
-        <Button color='primary' onClick={this.submitFeedbackToBackend()}>Submit</Button>
-      </Form>
+      <React.Fragment>
+        {this.state.alertStatus &&
+          <Alert color={this.state.alertStatus}>{this.state.alertMessage}</Alert>
+        }
+        <Form style={{ width: '70%' }}>
+          <FormGroup>
+            <Label>Your Name:</Label>
+            <Input
+              value={this.state.name}
+              onChange={event => this.onNameChange(event.target.value)}
+              type='text'
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Comments:</Label>
+            <Input
+              value={this.state.comments}
+              onChange={event => this.onCommentChange(event.target.value)}
+              type='textarea'
+            />
+          </FormGroup>
+          <Button color='primary' onClick={() => this.submitFeedbackToBackend()}>Submit</Button>
+        </Form>
+      </React.Fragment>
     );
   }
 }
